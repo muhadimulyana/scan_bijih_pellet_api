85ee99ba94d591e7060488fffde36df1a8a4ec97
muhadimulyana <muhammadadimulyana30@gmail.com>
2020-09-09 10:32:13 +0700

    menambahkan getlistbarcode (barcode hilang)

--- app/Http/Controllers/BarcodePelletDetController.php
+++ app/Http/Controllers/BarcodePelletDetController.php
@@ -18,9 +18,9 @@ class BarcodePelletDetController extends Controller
     
     public function getlistBarcode($pt, $gudang, $dep, $status, $notrans)
     {
-        //$gudang = urldecode($gudang);
+        $gudang = urldecode($gudang);
         $newstatus = $status == 'TERIMA' ? 'KIRIM' : 'KIRIM';
-        //$newpt = $pt == '1' ? 'ERA' : 'ERI'; 
+        $newpt = $pt == '1' ? 'ERA' : 'ERI'; 
 
         $result = DB::table('erasystem_2012.barcode_pellet')
             ->join('erasystem_2012.barcode_pellet_det', function ($join) {
@@ -28,14 +28,73 @@ class BarcodePelletDetController extends Controller
             })
             ->whereRaw('barcode_pellet_det.STATUS = ? AND barcode_pellet_det.NOTRANS = ? AND barcode_pellet.AKTIF = ?', [$newstatus, $notrans, '1'])
             ->selectRaw('barcode_pellet_det.BARCODE, barcode_pellet.KODE_PELLET, barcode_pellet.NAMA_PELLET, barcode_pellet.NAMA_LABEL, barcode_pellet.KG')
-            ->get();
+            ->get()->toArray();
+        
+        $bst = DB::table('erasystem_2012.bst_pellet')->where('NO_BST', $notrans)->first();
 
-        $out = [
-            'message' => 'success',
-            'result' => $result
-        ];
+        if(count($result) == $bst->TOTAL){
 
+            $out = [
+                'message' => 'success',
+                'result' => $result
+            ];
+        
+        } else {
+
+            $res = [];
+            $list_bst = DB::table('erasystem_2012.bst_pellet_item')->where('NO_BST', $notrans)->get();
+            $area = $bst->DARI_DEPT_AREA;
+
+            foreach($list_bst as $row){
+
+                $item = $row->KODE_PELLET;
+                $jml_item = $row->QTY;
+                
+                $barcode = DB::table('erasystem_2012.barcode_pellet')
+                    ->join('erasystem_2012.barcode_pellet_det', function ($join) {
+                        $join->on('barcode_pellet.BARCODE', '=', 'barcode_pellet_det.BARCODE')->on('barcode_pellet.LAST_UPDATE', '=', 'barcode_pellet_det.TANGGAL');
+                    })
+                    ->whereRaw('barcode_pellet_det.STATUS = ? AND barcode_pellet_det.NOTRANS = ? AND barcode_pellet.AKTIF = ? AND barcode_pellet.KODE_PELLET = ?', [$newstatus, $notrans, '1', $item])
+                    ->selectRaw('COUNT(*) AS JML_BARCODE')
+                    ->first();
+
+                $pellet = DB::table('erasystem_2012.barcode_pellet')
+                    ->join('erasystem_2012.barcode_pellet_det', function ($join) {
+                        $join->on('barcode_pellet.BARCODE', '=', 'barcode_pellet_det.BARCODE')->on('barcode_pellet.LAST_UPDATE', '=', 'barcode_pellet_det.TANGGAL');
+                    })
+                    ->whereRaw('barcode_pellet_det.PT_ID = ? AND barcode_pellet_det.GUDANG = ? AND barcode_pellet_det.DEPT_ID = ? AND barcode_pellet_det.DEPT_AREA = ? AND barcode_pellet_det.STATUS = ? AND barcode_pellet.KODE_PELLET = ? AND barcode_pellet.AKTIF = ?', [$newpt, $gudang, $dep, $area, 'KIRIM', $item, '1'])
+                    ->selectRaw("barcode_pellet.NAMA_LABEL, barcode_pellet.NAMA_PELLET, barcode_pellet.KODE_PELLET")
+                    ->first();
+
+                $jml_barcode = $barcode->JML_BARCODE;
+                $iterasi = $jml_item - $jml_barcode;
+
+                for( $i=1; $i <= $iterasi; $i++ ){
+
+                    $data[] = [
+                        'BARCODE' => $item . '/' . $i,
+                        'KODE_PELLET' => $item,
+                        'NAMA_PELLET' => $pellet->NAMA_PELLET,
+                        'NAMA_LABEL' => $pellet->NAMA_LABEL,
+                        'KG' => 25
+                    ];
+                }
+                
+
+            }
+
+            //$resdata = (object) $data;
+            $result = array_merge($result, $data);
+
+            $out = [
+                'message' => 'success',
+                'result' => $result
+            ];
+
+        }
+        
         return response()->json($out, 200, [], JSON_NUMERIC_CHECK);
+        
     }
 
     public function checkBarcode(Request $request)
