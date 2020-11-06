ce628350b7b3ead72849739117f62c03b1756bd9
muhadimulyana <muhammadadimulyana30@gmail.com>
2020-11-06 09:18:56 +0700

    change bst offline and fixing routes

---
+++ web.php
@@ -0,0 +1,140 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+ */
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+// Info Aplikasi
+$router->group(['prefix' => 'appInfo'], function () use ($router) {
+    $router->get('/getAppInfo/{packageName}', 'AppInfoController@show');
+    $router->post('/insert', 'AppInfoController@store');
+    $router->post('/checkUpdate', 'AppInfoController@checkUpdate');
+});
+
+// Login
+$router->post('/login', 'LoginController@login');
+
+// Cabang
+$router->group(['prefix' => 'cabang'], function () use ($router) {
+    $router->get('/', 'CabangController@index');
+    $router->get('/getPt', 'CabangController@getPt');
+    $router->get('/getGudang/{kaCab}', 'CabangController@getGudang');
+});
+
+// Info Device
+$router->group(['prefix' => 'deviceInfo'], function () use ($router) {
+    $router->get('/getDeviceInfo/{uDeviceId}/{uAppId}/{uName}', 'DeviceInfoController@show');
+    $router->post('/getDevice', 'DeviceInfoController@getDevice');
+    $router->post('/insert', 'DeviceInfoController@store');
+});
+
+// Rule Aplikasi
+$router->group(['prefix' => 'appRule'], function () use ($router) {
+    $router->get('/getRule/{uName}/{packageName}/{pageName}/{ruleName}', 'AppRuleController@show');
+    $router->post('/getRule', 'AppRuleController@getRule');
+    $router->post('/insert', 'AppRuleController@store');
+    $router->patch('/update', 'AppRuleController@update');
+});
+
+// Scan Bijih
+$router->group(['prefix' => 'barcodePelletDet'], function () use ($router) {
+    $router->get('/', 'BarcodePelletDetController@index');
+    //NOTE: route untuk mengambil barcode berdasarkan notrans
+    $router->get('/getBarcode/{notrans:[A-Za-z0-9/]+}', 'BarcodePelletDetController@getBarcode');
+    //Note : route untuk mengambil list barcode dengan inner join ke barcode pellet det
+    $router->get('/getlistBarcode/{pt}/{gudang}/{dep}/{status}/{notrans:[A-Za-z0-9/]+}', 'BarcodePelletDetController@getlistBarcode');
+    $router->post('/insert', 'BarcodePelletDetController@store');
+    $router->get('/getPellet', 'BarcodePelletDetController@getkodePellet');
+    $router->post('/checkBarcode', 'BarcodePelletDetController@checkBarcode');
+});
+
+//$router->get('/getBst', 'BstController@bstKirim');
+
+$router->group(['prefix' => 'bst'], function () use ($router) {
+    $router->get('/getBst/{pt}/{gudang}/{dept}/{area}', 'BstController@bstKirim');
+    $router->get('/getbstUser/{pt}/{dept}', 'BstController@getbstUser');
+    $router->get('/getDraf/{pt}/{gudang}/{dept}/{area}', 'BstController@getdrafBst');
+    $router->post('/insert', 'BstController@store');
+    $router->get('/getTotal/{notrans:[A-Za-z0-9/]+}', 'BstController@gettotalBarcode');
+    $router->get('/cekList/{notrans:[A-Za-z0-9/]+}', 'BstController@checklistBst');
+    $router->post('/terima', 'BstController@terimaBst');
+    $router->post('/update', 'BstController@update');
+    $router->post('/kirim', 'BstController@store');
+    $router->post('/checkBarcode', 'BstController@checkBarcode');
+    $router->post('/checkBarcodeKirim', 'BstController@checkBarcodeKirim');
+    $router->post('/checkBarcodeTerima', 'BstController@checkBarcodeTerima');
+    $router->post('/checkBarcodeKirimUpdate', 'BstController@checkBarcodeKirimUpdate');
+    $router->post('/ceklistArea', 'BstController@ceklistArea');
+    $router->delete('/delete/{notrans:[A-Za-z0-9/]+}/{userid}/{userdevice}', 'BstController@delete');
+    $router->get('/checkFinal/{notrans:[A-Za-z0-9/]+}', 'BstController@checkFinalize');
+    $router->get('/getlistBarcode/{pt}/{gudang}/{dep}/{status}/{notrans:[A-Za-z0-9/]+}', 'BstController@getlistBarcode');
+});
+
+//Departemen
+$router->group(['prefix' => 'dept'], function () use ($router) {
+    $router->get('/', 'DepartemenController@index');
+    $router->get('/deptScan', 'DepartemenController@getdeptScan');
+    $router->get('/deptPengirim/{user}/{dept}', 'DepartemenController@getdeptPengirim');
+});
+
+//Area
+$router->group(['prefix' => 'area'], function () use ($router) {
+    $router->get('/show/{dept}', 'StrukturJabatanController@show');
+    $router->get('/getArea/{user}', 'StrukturJabatanController@getArea');
+    //$router->get('/deptScan', 'DepartemenController@getdeptScan');
+});
+
+//Router DO
+$router->group(['prefix' => 'doi'], function() use ($router){
+    $router->get('/', 'DoiController@test');
+    $router->get('/getlistdoiUser/{pt}/{gudang}', 'DoiController@getlistuserDoi'); //Sesuaikan
+    $router->get('/getlistDoi/{pt}/{gudang}', 'DoiController@getlistDoi');
+    $router->get('/getlistBarcode/{IdDo:[A-Za-z0-9/]+}', 'DoiController@getlistBarcode');
+    $router->get('/getlistbarcodeUpdate/{IdDo:[A-Za-z0-9/]+}', 'DoiController@getlistbarcodeUpdate');
+    $router->get('/getDraf/{pt}/{gudang}', 'DoiController@getdrafDoi');
+    $router->get('/getTotal/{IdDo:[A-Za-z0-9/]+}', 'DoiController@gettotalBarcode');
+    $router->post('/terima', 'DoiController@terimaDoi');
+    // 
+    $router->get('/getSoi/{pt}/{gudang}', 'DoiController@getSo');
+    $router->get('/getJadwal/{pt}/{gudang}', 'DoiController@getJadwal');
+    $router->post('/checkBarcode', 'DoiController@checkBarcode');
+    $router->post('/checkBarcodeKirim', 'DoiController@checkBarcodeKirim');
+    $router->post('/checkBarcodeTerima', 'DoiController@checkBarcodeTerima');
+    $router->post('/checkbarcodeUpdate', 'DoiController@checkbarcodeUpdate');
+    $router->get('/checkItem/{idSo:[A-Za-z0-9/]+}/{kode}/{total}', 'DoiController@checktotalItem');
+    $router->post('/kirim', 'DoiController@store');
+    $router->post('/update', 'DoiController@update');
+    $router->delete('/delete/{notrans:[A-Za-z0-9/]+}', 'DoiController@delete');
+    $router->get('/checkFinal/{notrans:[A-Za-z0-9/]+}', 'DoiController@checkFinalize');
+    $router->get('/getlistBarcodeKirim/{IdSo:[A-Za-z0-9/]+}', 'DoiController@getlistBarcodeKirim');
+});
+
+//User Routes
+$router->group(['prefix' => 'users'], function () use ($router) {
+    $router->get('/checkPin/{username}/{pin}', 'UsersController@checkPin');
+    //$router->get('/deptScan', 'DepartemenController@getdeptScan');
+});
+
+//Tutor Routes
+$router->group(['prefix' => 'appTut'], function () use ($router) {
+    $router->post('/getTutor', 'AppTutController@getTutor');
+    //$router->get('/deptScan', 'DepartemenController@getdeptScan');
+});
+
+
+//Example Routes
+$router->group(['prefix' => 'exam'], function () use ($router) {
+    $router->get('/test/{id:.*', 'ExampleController@testRoute');
+    //$router->get('/deptScan', 'DepartemenController@getdeptScan');
+});
